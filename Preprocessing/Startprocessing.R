1545560e36ab8aca88d16c09dd7357bc0e1f859c
Dean <deansteven308@gmail.com>
2014-09-11 23:50:11 +1000

    Python PDF manipulation
    Stage 1 - Reading into string format complete

--- Preprocessing/Startprocessing.R
+++ Preprocessing/Startprocessing.R
@@ -1,2 +1,14 @@
 # Preprocessing step will convert to modual at some point in the future.
+# Text mining package I think
+
+exe <- "C:/Program Files/xpdf64/pdftotext.exe"
+path <- "E:/TestData/1ch speech NMF.pdf"
+
+require(tm)
+
+file <- readPDF(path)
+# Inputfile <- system(paste(exe,path, wait=FALSE))
+
+print(Inputfile)
+system
 
---
+++ Preprocessing/pdf reading into corpus.R
@@ -0,0 +1,4 @@
+#Read files test 
+
+stuff <- list.dirs(path = sy, full.names = FALSE, recursive = TRUE)
+
