9717576717590c213582a70018983d35ddb510eb
Vaccuss <Deansteven308@gmail.com>
2014-10-18 22:34:20 +1000

    txt file prep

--- Preprocessing/PDFmanipulation/PDFManipulation.py
+++ Preprocessing/PDFmanipulation/PDFManipulation.py
@@ -2,22 +2,25 @@ __author__ = 'Dean'
 
 
 import glob
+import csv
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from cStringIO import StringIO
 from pdfminer.layout import LAParams
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdftypes import resolve1
+from xmp import xmp_to_dict
 from pdfminer.converter import TextConverter
 
+
+'''"C:\Users\Dean\Documents\TestPDF's" +'''
+
 def get_file_paths():
-    sysPath = "C:\Users\Dean\Documents\TestPDF's" + "\*.pdf"
-    fileArray = glob.glob(sysPath)
-    for f in fileArray:
-        try:
-            return fileArray
-        except ValueError:
-            print 'big error dude'
-        except IndexError:
-            print "File Name incorrect"
+    sysPath = "\*.pdf"
+    user_input = raw_input("Enter the document paths:")
+    fullPath = user_input + sysPath
+    fileArray = glob.glob(fullPath)
+    return fileArray
 
 def convert_pdf_to_text(path):
     resourceManager = PDFResourceManager()
@@ -39,20 +42,41 @@ def convert_pdf_to_text(path):
     retstr.close()
     return str
 
+def get_title_metadata(file_path):
+    fp = open('mypdf.pdf', 'rb')
+    parser = PDFParser(fp)
+    doc = PDFDocument()
+    parser.set_document(doc)
+    doc.set_parser(parser)
+    doc.initialize()
 
-def str_processing():
-    path_array = get_file_paths()
+    print doc.info        # The "Info" metadata
+
+    if 'Metadata' in doc.catalog:
+        metadata = resolve1(doc.catalog['Metadata']).get_data()
+        print xmp_to_dict(metadata)
+
+
+def str_processing(file):
     word_list = []
-    for file in path_array:
-        strfile = convert_pdf_to_text(file)
-        replace_chars = ['-', ':', ',', '.', '(', ')', "1", '2', '3','4', '5', '6', '7', '8', '9', '0']
-        for i in replace_chars:
-            strfile = strfile.replace(i, ' ')
-        strfile = strfile.lower()
-        processed_information = strfile.split()
-        word_list.append(processed_information)
-    print(word_list)
+    strfile = convert_pdf_to_text(file)
+    replace_chars = ['-', ':', ',', '.', '(', ')', "1", '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    for i in replace_chars:
+        strfile = strfile.replace(i, ' ')
+    strfile = strfile.lower()
+    processed_information = strfile.split()
+    word_list.append(processed_information)
+    return word_list
 
 
 if __name__ == "__main__":
-    str_processing()
+    file_path_array = get_file_paths()
+    for f in file_path_array:
+        text = str_processing(f)
+        title = get_title_metadata(f)
+        csv_file = open(title, wb)
+        writer = csv.writer(csv_file)
+        writer.writerows(text)
+        csv_file.close()
+
+
